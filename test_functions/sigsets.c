f0bacba89eeaf15deb202a54726f66aff51f39c0
mohamad <zolfagharipour@gmail.com>
2024-01-04 10:46:51 +0100

    test files added

--- test_functions/sigsets.c
+++ test_functions/sigsets.c
@@ -13,4 +13,7 @@ int main()
 	// now SIGINT is added to signalset, i can not find any use-case for it 
 	// without using fuctions like sigismember() or sigprocmask()
 
+
+	// update: I asked vitali how this has to be used and he said there is no need and signal (sigaction) is all you need.
+
 }

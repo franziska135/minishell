d395c917d4ae6ec55738d44bf987fa2fe3c6ac94
Mohamad Zolfaghari Pour <mzolfagh@c3r9p7.42vienna.com>
2024-01-07 19:13:18 +0100

    utils1 added

---
+++ utils1.c
@@ -0,0 +1,32 @@
+#include "minishell.h"
+
+void	dpointer_free(char **str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	free(str);
+}
+
+void	struct_free(t_compound cmds)
+{
+	int		i;
+
+	i = 0;
+	while (i < cmds.nbr_scmd)
+	{
+		dpointer_free(cmds.scmd[i].cmd);
+		i++;
+	}
+	free(cmds.scmd);
+}
+void	err_handler(char *str)
+{
+	printf("%s\n", str);
+}
+

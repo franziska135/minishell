dc2647c4a0f747247a6f3af4b1f2586b323be1f0
mohamad <zolfagharipour@gmail.com>
2024-01-16 16:34:29 +0100

    env init is added from fmarggra

---
+++ builtin_env.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmarggra <fmarggra@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 17:10:22 by fmarggra          #+#    #+#             */
+/*   Updated: 2023/12/15 17:10:23 by fmarggra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "execute.h"
+
+void	init_path_struct(t_execute *execute)
+{
+	execute->binary_paths = NULL;
+	execute->amt_children = 0;
+	execute->amt_pipes = 0;
+}
+
+//returns a pointer to the node storing the variable the variable
+//returns NULL if variable not found
+t_env	*find_node(t_compound *cmds, char *needle)
+{
+	t_env	*haystack;
+	char	*str;
+
+	haystack = cmds->env_ll;
+	while (haystack)
+	{
+		if (ft_strncmp(haystack->key, needle, ft_strlen(needle)) == 0)
+			return (haystack);
+		haystack = haystack->next;
+	}
+	return (NULL);
+}
+
+//make sure that nothing gets updated in case the variable doesnt exist
+//check for if new value == NULL;
+////!!!!!CHECK FOR NEW VALUIE == NULL;
+//error if node is unset and not found?
+void	update_env_ll(t_compound *cmds, char *variable, char *new_value)
+{
+	t_env	*node;
+
+
+	if (new_value && variable)
+	{
+		node = find_node(cmds, variable);
+  		if (node)
+		{
+    		printf("old value of %s: \t%s\n", variable, node->value);
+			free(node->value);
+			node->value = ft_strdup(new_value);
+			printf("new  value of %s: \t%s\n\n", variable, node->value);
+		}
+	}
+}
+
+//splits the path under PATH=to check 
+void	split_binary_paths(t_execute *execute, t_compound *cmds)
+{
+	t_env	*node;
+
+	node = find_node(cmds, "PATH=");
+	if (node && node->value != NULL)
+	execute->binary_paths = ft_split(node->value, ':');
+	// if (!execute->binary_paths)
+		// (free (path_str), path_str = NULL);
+		// 	free everything for the current process, return to history readline;
+	// print_paths(execute);
+}
+
+//can be taken out later
+void	print_paths(t_execute *execute)
+{
+	int	i;
+
+	i = 0;
+	while (execute->binary_paths[i] != NULL)
+	{
+		printf("%s\n", execute->binary_paths[i]);
+		i++;
+	}
+}
---
+++ main_env_ll.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fmarggra <fmarggra@student.42vienna.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 17:10:22 by fmarggra          #+#    #+#             */
+/*   Updated: 2023/12/15 17:10:23 by fmarggra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "execute.h"
+
+//add checks for key/value = NULL -> what would that mean?
+//if one of them null, no variable value??
+t_env	*ft_new_env_node(char *key, char *value)
+{
+	t_env	*ptr;
+
+	ptr = (t_env *)malloc(sizeof(t_env));
+	if (!(ptr))
+		return (NULL);
+	ptr->key = key;
+	ptr->value = value;
+	// printf("key:%s\n", ptr->key);
+	// printf("value:%s\n", ptr->value);
+	ptr->next = NULL;
+	return (ptr);
+}
+
+void	ft_add_last_node(t_env **lst, t_env *new)
+{
+	t_env	*tmp;
+
+	tmp = *lst;
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
+}
+
+//find the = delimiter for key and value to split them
+size_t	iterate_ultil_equal(const char *envp_i)
+{
+	int	i;
+
+	i = 0;
+	while (envp_i[i] && envp_i[i] != '=')
+		i++;
+	return (i + 1);
+}
+
+//a copy of the environment stored in a linked list
+//add NULL checks for substr and new_env_node
+void	init_env_llist(t_compound *cmds, char **envp)
+{
+	t_env	*new_node;
+	t_env	*head;
+	char	*key;
+	char	*value;
+	size_t	i_equal_sign;
+
+	key = NULL;
+	value = NULL;
+	int i;
+
+	head = NULL;
+	i = 0;
+	while(envp[i])
+	{
+		i_equal_sign = iterate_ultil_equal(envp[i]);
+		key = ft_substr(envp[i], 0, i_equal_sign);
+		value = ft_substr(envp[i], i_equal_sign, ft_strlen(envp[i]) - i_equal_sign);
+		new_node = ft_new_env_node(key, value);
+		ft_add_last_node(&head, new_node);
+		i++;
+	}
+	//i cant free key and value can i?
+	//i can free if i use strdup on both every time, but that creates even more trouble doesnt it?
+	free (key);
+	cmds->env_ll = head;
+}

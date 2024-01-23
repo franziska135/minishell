60fbf00fe1496f01b6c8761710c825787e21ed5b
mohamad <zolfagharipour@gmail.com>
2024-01-23 17:30:01 +0100

    fds are no longer open, piping needs more testing

--- pipin_root.c
+++ pipin_root.c
@@ -1,9 +1,10 @@
 #include "minishell.h"
 
-static int	child_proccess(t_compound *cmds, int *fd, int i)
+static int	child_proccess(t_compound *cmds, int *fd, int i, int initial_stdin)
 {
 	char	*path;
 
+	close (initial_stdin);
 	if (cmds->scmd[i].in_fd != -1 && cmds->scmd[i].out_fd != -1)
 	{
 		if (cmds->scmd[i].out_fd != 0)
@@ -33,7 +34,9 @@ static int	piping(t_compound *cmds)
 	int	fd[2];
 	int	i;
 	int	pid;
+	int	initial_stdin;
 
+	initial_stdin = dup(STDIN_FILENO);
 	i = 0;
 	while (i < cmds->nbr_scmd)
 	{
@@ -46,7 +49,7 @@ static int	piping(t_compound *cmds)
 		}
 		pid = fork();
 		if (pid == 0)
-			child_proccess(cmds, fd, i);
+			child_proccess(cmds, fd, i, initial_stdin);
 		close (fd[1]);
 		if (i < (cmds->nbr_scmd - 1) && cmds->scmd[i + 1].in_fd == 0 && cmds->scmd[i].out_fd == 0)
 			dup2(fd[0], STDIN_FILENO);
@@ -57,6 +60,8 @@ static int	piping(t_compound *cmds)
 	}
 	if (waitpid(pid, NULL, 0) == -1)
 		return(0);
+	dup2(initial_stdin, STDIN_FILENO);
+	close (initial_stdin);
 	close (fd[0]);
 	close (fd[1]);
 	return (1);
@@ -64,19 +69,13 @@ static int	piping(t_compound *cmds)
 
 int	piping_root(t_compound *cmds)
 {
-	int	initial_stdin;
-	int	initial_stdout;
-
-	initial_stdin = dup(STDIN_FILENO);
-	// initial_stdout = dup(STDOUT_FILENO);
 	if (!piping(cmds))
 	{
 		dup2(0, STDIN_FILENO);
 		dup2(1, STDOUT_FILENO);
 		return (0);
 	}
-	dup2(initial_stdin, STDIN_FILENO);
-	// dup2(initial_stdout, STDOUT_FILENO);
+
 	return (1);
 
 }

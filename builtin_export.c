74372a2c82f3992203a135c0fd205022b79357bc
franziska135 <144387795+franziska135@users.noreply.github.com>
2024-01-27 18:42:07 +0000

    changed the export function

--- builtin_export.c
+++ builtin_export.c
@@ -36,12 +36,12 @@ void	print_export(t_env *head)
 	{
 		write (1, "dexlare -x ", 11);
 		write(1, current->key, ft_strlen(current->key));
-		if (current->value)
-		{
-			write (1, "=", 1);
-			//printf("%s", current->value);
-			write(1, current->value, ft_strlen(current->value));
-		}
+		// if (current->value)
+		// {
+		// 	write (1, "=", 1);
+		// 	//printf("%s", current->value);
+		// 	write(1, current->value, ft_strlen(current->value));
+		// }
 		write (1, "\n", 1);
 		current = current->next;
 	}
@@ -50,62 +50,219 @@ void	print_export(t_env *head)
 	//newline at the end?
 }
 
+char	*save_key(char *cmd1)
+{
+	int		i;
+	char	*key;
+
+	key = NULL;
+	i = 0;
+
+	while (cmd1[i] != '=' && cmd1[i] != '\0')
+		i++;
+	key = (char *)malloc(sizeof(char) * (i + 1));
+	if (!key)
+		return (NULL);
+	i = 0;
+	while (cmd1[i] != '=' && cmd1[i] != '\0')
+	{
+		key[i] = cmd1[i];
+		i++;
+	}
+	key[i] = '\0';
+	return (key);
+}
+
+//returns null if no value or if no equal sing;
+char	*save_value(char *cmd1)
+{
+	int		i;
+	int		lenght_value;
+	char	*value;
+
+	value = NULL;
+	i = 0;
+	lenght_value = 0;
+	while (cmd1[i] != '=' && cmd1[i] != '\0')
+		i++;
+	// if (cmd1[i] == '\0')
+	// 	return (NULL);
+	// else if (cmd1[i] == '=' && cmd1[i + 1] == '\0')
+	// 	return (NULL)
+	i++;
+	while (cmd1[i + lenght_value] != '\0')
+		lenght_value++;
+	value = (char *)malloc(sizeof(char) * (lenght_value + 1));
+	if (!value)
+		return (NULL);
+	while (cmd1[i] != '\0')
+	{
+		value[i] = cmd1[i];
+		i++;
+	}
+	value[i] = '\0';
+	return (value);
+}
+
+int	equal_sign_and_value(char *cmd1)
+{
+	int i;
+
+	while (cmd1[i] != '\0' && cmd1[i] != '=')
+		i++;
+	if (cmd1[i] == '\0')
+		return (0); //no equal sign
+	if (cmd1[i] == '=' && cmd1[i + 1] == '\0')
+		return (1); //equal sign but no value;
+	else
+		return (2); //equal sign and value
+}
+
+//check the find node function, it doesnt check for C
+//error checking
 void	builtin_export(t_compound *cmds, t_simple *scmd)
 {
 	int		i;
 	t_env	*new_node;
+	char	*key;
+	char	*value;
 
 	i = 0;
+	key = NULL;
+	value = NULL;
 	if (!scmd->cmd[1])
+	{
 		print_export(cmds->env_ll);
+		return ;
+	}
 	else if (ft_strncmp(scmd->cmd[1], "=\0", 2) == 0)
 		print_error("minishell: ", "export: ", "'='", "not a valid identifier");
 	//create node if variable does not exist yet
-	else if (find_node(cmds, scmd->cmd[1]) == NULL)
+	key = save_key(scmd->cmd[1]);
+	//error if malloc failed
+	if (!key)
+		return ;
+	if (equal_sign_and_value(scmd->cmd[1]) == 2) // 2 if there is value
 	{
-		write (1, "\ncheck1\n", 8);
-		new_node = ft_new_env_node(scmd->cmd[1], NULL, FALSE);
-		//check if there is an assignment, if there is 
-		if (scmd->cmd[2] && ft_strncmp(scmd->cmd[2], "=\0", 2) == 0)
-		{
-			if (!scmd->cmd[3])
-			{
-				new_node->value = ft_strdup("\0");
-				write (1, "new empty val added to env and export\n\n", 39);
-			}
-			else
-			{
-		 	  	new_node->value = ft_strdup(scmd->cmd[3]);
-				write (1, "new full val added to env and export\n\n", 38);
-			}
-		 	new_node->env_display = TRUE;
-		}
-		else
-			write (1, "new empty val added only to export\n", 37);
-		ft_add_last_node(&cmds->env_ll, new_node);
+		value = save_value(scmd->cmd[1]);
+		//return if malloc failed --> if null returend
+		if (!value)
+			return ;
 	}
-	//if node exists
-	else
+	//if variable exists
+	if (find_node(cmds, key) != NULL)
 	{
-		write (1, "\ncheck2\n", 8);
-		new_node = find_node(cmds, scmd->cmd[1]);
-		//check if there is an assignment
-		if (scmd->cmd[2] && ft_strncmp(scmd->cmd[2], "=\0", 2) == 0)
+		new_node = find_node(cmds, key);
+		//if equal sign --> return not 0
+		if (equal_sign_and_value(scmd->cmd[1]) != 0)
 		{
-			if (new_node->value)
-			 	free(new_node->value);	
-			if (!scmd->cmd[3])
+			//if equal sign and no value --> return 1
+			if (equal_sign_and_value(scmd->cmd[1]) == 1)
 			{
-				new_node->value = ft_strdup("\0");
-				write (1, "existing val set to null in env and export\n\n", 45);
+				//value = \0;
+				new_node->value = '\0';
+				//display flag 1 --> show in export and env
+				new_node->env_display = TRUE;
+				write (1, "existing value set to a null terminator in env and export\n", 59);
 			}
-			else
+			//if equal sign and value --> return 2
+			else if (equal_sign_and_value(scmd->cmd[1]) == 2)
 			{
-				new_node->value = ft_strdup(scmd->cmd[3]);
+				//value = value
+				new_node->value = value;
+				//display flag 1
+				new_node->env_display = TRUE;
 				write (1, "existing val set to new value in env and export\n\n", 49);
 			}
 		}
+		//else: if no equal sign nothing happens
 	}
+	//if node does not exist yet
+	else
+	{
+		//create new node with key = key
+		new_node = ft_new_env_node(key, NULL, TRUE);
+		//if no equal sign
+		if (equal_sign_and_value(scmd->cmd[1]) == 0)
+		{
+			//key = key; -> already set, no action
+			//value = NULL --> alrady set, no action
+			//diplay flag = FALSE;
+			new_node->env_display = FALSE;
+			write (1, "new empty val added only to export\n", 37);
+		}
+		//if equal sign and no value -> return 1
+		if (equal_sign_and_value(scmd->cmd[1]) == 1)
+		{
+			//key = key --> already set, no action
+			//value = \0
+			new_node->value = '\0';
+			//diplay flag 1 --> export and env --> already set
+			write (1, "new empty val added to env and export\n\n", 39);
+		}
+		//if equal sign and value -> return 2
+		else if (equal_sign_and_value(scmd->cmd[1]) == 2)
+		{
+			//key = key; -_> already, no action
+			//value = value
+			new_node->value = value;
+			//display flag 1 --> export and env --> already set
+			write (1, "new full val added to env and export\n\n", 38);
+		}
+		ft_add_last_node(&cmds->env_ll, new_node);
+	}
+	printf("check");
+	// (free(key), free(value));
+	// key = NULL;
+	// value = NULL;
 	//print_export(cmds->env_ll);
-	//builtin_env(cmds->env_ll);
 }
+// 	else if (find_node(cmds, needle) == NULL)
+// 	{
+// 		write (1, "\ncheck1\n", 8);
+// 		new_node = ft_new_env_node(scmd->cmd[1], NULL, FALSE);
+// 		//check if there is an assignment, if there is 
+// 		if (scmd->cmd[2] && ft_strncmp(scmd->cmd[2], "=\0", 2) == 0)
+// 		{
+// 			if (!scmd->cmd[3])
+// 			{
+// 				new_node->value = ft_strdup("\0");
+// 				write (1, "new empty val added to env and export\n\n", 39);
+// 			}
+// 			else
+// 			{
+// 		 	  	new_node->value = ft_strdup(scmd->cmd[3]);
+// 				write (1, "new full val added to env and export\n\n", 38);
+// 			}
+// 		 	new_node->env_display = TRUE;
+// 		}
+// 		else
+// 			write (1, "new empty val added only to export\n", 37);
+// 		ft_add_last_node(&cmds->env_ll, new_node);
+// 	}
+// 	//if node exists
+// 	else
+// 	{
+// 		write (1, "\ncheck2\n", 8);
+// 		new_node = find_node(cmds, scmd->cmd[1]);
+// 		//check if there is an assignment
+// 		if (scmd->cmd[2] && ft_strncmp(scmd->cmd[2], "=\0", 2) == 0)
+// 		{
+// 			if (new_node->value)
+// 			 	free(new_node->value);	
+// 			if (!scmd->cmd[3])
+// 			{
+// 				new_node->value = ft_strdup("\0");
+// 				write (1, "existing val set to null in env and export\n\n", 45);
+// 			}
+// 			else
+// 			{
+// 				new_node->value = ft_strdup(scmd->cmd[3]);
+// 				write (1, "existing val set to new value in env and export\n\n", 49);
+// 			}
+// 		}
+// 	}
+// 	
+// 	//print_export(cmds->env_ll);
+// 	//builtin_env(cmds->env_ll);
+// }
